225d16be06ff099c9b0d71b915b7f0f5ba2a9ea2
c-ostic <60513574+c-ostic@users.noreply.github.com>
2023-09-29 17:42:16 -0400

    Added login
    Added functionality to login route
    - allows users to login with username password
    - user will be returned with jwt
    
    Added test route for jwt
    - returns Welcome! is valid jwt is provided, returns Unauthorized otherwise

--- controllers/user.js
+++ controllers/user.js
@@ -41,5 +41,36 @@ exports.createUser = async (req, res) => {
 };
 
 exports.login = async (req, res) => {
-  
+  try {
+    // Get user input
+    const { username, password } = req.body;
+
+    // Validate user input
+    if (!(username && password)) {
+      res.status(400).send("Username and password required");
+    }
+
+    // Validate if user exist in our database
+    const user = await User.findOne({ username });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // Create token
+      const token = jwt.generateToken(user)
+
+      // save user token
+      user.token = token;
+
+      // user
+      res.status(200).json(user);
+      return
+    }
+
+    res.status(400).send("Invalid Credentials");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+exports.welcome = async (req, res) => {
+  res.status(200).send('Welcome!')
 }
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,6 +2,11 @@ const { verifyToken } = require('../auth');
 
 exports.isAuthenticated = (req, res, next) => {
   const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(403).send("A token is required for authentication");
+  }
+
   try {
     const decoded = verifyToken(token);
     req.user = decoded;
