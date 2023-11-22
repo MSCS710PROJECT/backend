93ae492dae7d19c961bb90af96a62804879062b8
c-ostic <60513574+c-ostic@users.noreply.github.com>
2023-11-22 13:10:18 -0500

    Chatbot Attractions and Alerts
    Added two more routes
    /bot/attractions - takes a string start and end location and provides a list of 10 tourist attractions between those locations (provided by chatbot)
    /emailAlert - takes message and sends the user an email with the message
    
    Updated openapi YAML

--- src/controllers/chatbot.js
+++ src/controllers/chatbot.js
@@ -29,3 +29,37 @@ exports.getResponse = async (req, res) => {
         res.status(500).json({ message: "Sorry, I couldn't fetch the weather information." });
     }
 }
+
+exports.getTouristAttractions = async (req, res) => {
+    const { start, end } = req.body;
+
+    // Check if the message is related to weather
+    // if (!userMessage.toLowerCase().includes('weather')) {
+    //     return res.json({ message: "I can only provide weather information." });
+    // }
+
+    const message = `What are 10 tourist attractions on the route between ${start} and ${end}? Format the answer in a numbered list.`
+
+    try {
+        const response = await axios.post(OPENAI_API_URL, {
+            prompt: message,
+            max_tokens: 150
+        }, {
+            headers: {
+                'Authorization': `Bearer ${OPENAI_API_KEY}`,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const chatGPTResponse = response.data.choices[0].text.trim();
+        const attractionArray = chatGPTResponse.split(/\s*\d+\.\s*/);
+        attractionArray.shift();
+
+        //console.log(attractionArray);
+
+        res.json({ locations: attractionArray });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Sorry, I couldn't fetch route attractions." });
+    }
+}
